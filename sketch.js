67ed8df56bbdc67293758cfe87fa5d01fb10cac7
Jagmeet5100 <109854062+Jagmeet5100@users.noreply.github.com>
2022-07-23 16:43:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,6 @@
+
+console.log("hello");
+console.log("hello");
+console.log("My bame is Jgmeet singh");
+console.log("My school name is Rayat bahra international school");
+console.log("My address is bullawari chownk near pattanjali store ");
